162295a1f3533bc3269f1c99d52174e0fb3c47ee
Aditya2743 <aadityaryadav2002@gmail.com>
2024-02-23 00:42:28 +0530

    om sawant

--- queue.cpp
+++ queue.cpp
@@ -8,7 +8,22 @@ int main(){
     queue<int> numbers;
     queue<string> alphabets;
 
+    numbers.push(1);
+    numbers.push(2);
+    numbers.push(3);
 
+    while(!numbers.empty()){
+        cout << numbers.front() << " ";
+        numbers.pop();
+    }
     
     return 0;
 }
+
+
+
+/*
+    queue operations are ->
+    PUSH, POP, FRONT, BACK, ISEMPTY, SIZE
+    https://www.programiz.com/cpp-programming/queue
+*/
